afb377e199a52a0392fa42ef4cb4cd5a7c6ec7b8
IT24300312 <it24300312@my.sliit.lk>
2024-11-08 21:57:10 +0530

    Add files via upload

---
+++ Q1A/IT24300312Lab2Q1.java
@@ -0,0 +1,20 @@
+public class IT24300312Lab2Q1 {
+
+     public static void main(String[] args)
+{
+     int perimeter = 100;
+     
+     double length = perimeter * 2.0 / 7.0;
+    
+     double width = length * 3.0 / 4.0;
+
+     System.out.println("length: " +
+length);
+     
+     System.out.println("width: " +
+width);
+
+
+       }
+
+} 
